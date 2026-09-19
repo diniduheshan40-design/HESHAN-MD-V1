e1909e1af7c5677e9198769f7481a848a61e04b6
diniduheshan40-design <diniduheshan40@gmail.com>
2026-09-19 19:13:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://diniduheshan40_db_user:nziBElbMabqIwsol@cluster0.5gazebm.mongodb.net/?appName=Cluster0',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://diniduheshan40_db_user:Heshan2007@cluster0.5gazebm.mongodb.net/?appName=Cluster0',
   BOT_NAME: 'HESHAN MD V1',
   OWNER_NUMBER: '94719845166'
 };
